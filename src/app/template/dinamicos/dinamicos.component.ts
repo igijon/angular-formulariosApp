0c82710cb686d08e70f051decec564999b034631
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-06-18 10:05:41 +0200

    Template: agregar juegos dinámicamente

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -24,6 +24,17 @@ export class DinamicosComponent {
       { id: 2, nombre: 'DeathStranding'},
     ]
   };
+  nuevoJuego: string = '';
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      nombre: this.nuevoJuego,
+      id: this.persona.favoritos.length + 1
+      //Los id vienen de BDD por lo qu estoy es sólo un ejemplo
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego='';
+  }
 
   eliminar(index: number) {
     this.persona.favoritos.splice(index, 1);
@@ -31,6 +42,5 @@ export class DinamicosComponent {
 
   guardar() {
     console.log('Formulario posteado');
-
   }
 }
