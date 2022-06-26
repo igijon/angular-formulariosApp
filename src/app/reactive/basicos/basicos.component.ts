159b0e19b99aa7470c49258acb584093ae7f9f92
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-06-26 18:08:29 +0200

    Validaciones básicas: mensajes de error

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -16,11 +16,15 @@ export class BasicosComponent {
   // });
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
-    precio: [0, [Validators.required, Validators.min(0)]],
-    existencias: [0, [Validators.required, Validators.min(0)]]
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]]
   })
 
   constructor( private fb: FormBuilder) { }
 
+  campoNoValido(nombreControl: string) {
+    return this.miFormulario.controls[nombreControl].errors
+        && this.miFormulario.controls[nombreControl].touched;
+  }
 }
